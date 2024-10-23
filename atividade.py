29a0303fb5b032bf5b03f733faa2a49b4b390027
SofiaPontes <santospontessofia@gmail.com>
2024-10-23 17:10:35 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,7 @@
+nome = input("Insira seu nome")
+idade = input("Insira sua idade")
+estudante = input("Você é estudante?")
+
+resultado = " Eu me chamo" + nome + " eu tenho" + idade + " anos" + " e" + estudante + " sou estudante " 
+
+print(resultado)
